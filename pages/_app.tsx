f13af0a17c25b8e798920ef97ce0a1fa7cf6e343
fimac <fimccawley@gmail.com>
2025-03-10 19:03:44 +1100

    Fix up links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,45 +11,37 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           <nav className='relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg mb-3'>
             <div className='container mx-auto flex items-center justify-between'>
               <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
-                <Link href='/'>
-                  <a className='text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-gray-600'>
-                    <span className='text-pink-500'>F</span>
-                    <span className='text-blue-500'>M</span>
-                  </a>
+                <Link href='/' className='text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-gray-600'>
+                  <span className='text-pink-500'>F</span>
+                  <span className='text-blue-500'>M</span>
                 </Link>
               </div>
               <div className='flex items-center' id='example-navbar-warning'>
                 <ul className='flex flex-row list-none ml-auto'>
                   <li className='nav-item'>
-                    <Link href='/about'>
-                      <a className='px-3 py-2 flex items-center text-2xl uppercase leading-snug text-gray-600 hover:opacity-75'>
-                        About
-                      </a>
+                    <Link href='/about' className='px-3 py-2 flex items-center text-2xl uppercase leading-snug text-gray-600 hover:opacity-75'>
+                      About
                     </Link>
                   </li>
                   <li className='nav-item'>
-                    <Link href='/talks'>
-                      <a className='px-3 py-2 flex items-center text-2xl uppercase leading-snug text-gray-600 hover:opacity-75'>
-                        Talks
-                      </a>
+                    <Link href='/talks' className='px-3 py-2 flex items-center text-2xl uppercase leading-snug text-gray-600 hover:opacity-75'>
+                      Talks
                     </Link>
                   </li>
                   <li className='nav-item'>
-                    <Link href='/work-history'>
-                      <a className='px-3 py-2 flex items-center text-2xl uppercase leading-snug text-gray-600 hover:opacity-75'>
-                        Work
-                      </a>
+                    <Link href='/work-history' className='px-3 py-2 flex items-center text-2xl uppercase leading-snug text-gray-600 hover:opacity-75'>
+                      Work
                     </Link>
                   </li>
                   <li className='nav-item'>
-                    <a
+                    <Link
                       href='/fionamccawley_resume.pdf'
                       className='px-3 py-2 flex items-center text-2xl uppercase leading-snug text-gray-600 hover:opacity-75'
                       target='_blank'
                       rel='noopener noreferrer'
                     >
                       Resume
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </div>
